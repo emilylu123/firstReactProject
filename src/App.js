fe481049661fbfb9d15bd0af815a95c45df6a36f
Emily Lu <emilylu123@gmail.com>
2021-01-13 21:15:03 +1030

    Components Part 2 - menu dishes with card

--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import { Navbar, NavbarBrand } from 'reactstrap';
+import { DISHES } from "./shared/dishes";
 import Menu from './components/MenuComponent';
 import './App.css';
 
 class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            dishes:DISHES
+        };
+    }
   render() {
     return (
         <div className="App">
-          <Navbar dark color="primary">
-            <div className="container">
-              <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-            </div>
-          </Navbar>
-        <Menu/>
+            <Navbar dark color="primary">
+                <div className="container">
+                    <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+                </div>
+            </Navbar>
+            <Menu dishes={this.state.dishes} />
         </div>
     );
   }
